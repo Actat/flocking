fc8586a7c1cce9e387738e3df9c7048a4a29e313
Actat <akiotoshima@gmail.com>
2016-11-09 23:56:07 +0900

    modified:   main.c コメントを増やした

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 
+/*
+ * コマンドライン引数
+ * argv[0] プログラムへのパス
+ * argv[1] max_step 出力するファイルの個数
+ */
+
 int main(int argc, char* argv[])
 {
     if (argc != 2) {
@@ -15,17 +21,24 @@ int main(int argc, char* argv[])
     double small_fish[fish_amount][3][2]; // fish0[[x, y], [v_x, v_y], [\omega, \alpha]]
 
     for (int i = 0; i < max_step; i++) {
+        // 移動の処理
         for (int j = 0; j < fish_amount; j++) {
+            // 並進
             small_fish[j][0][0] += small_fish[j][1][0]; // x = x + v_x
             small_fish[j][0][1] += small_fish[j][1][1]; // y = y + v_y
 
-            // 並進速度に\omega回る回転行列をかける
+            // \omega回る回転行列をかける
             small_fish[j][1][0] = cos(small_fish[j][2][0]) * small_fish[j][1][0] + sin(small_fish[j][2][0]) * small_fish[j][1][1];
             small_fish[j][1][1] = -1 * sin(small_fish[j][2][0]) * small_fish[j][1][0] + cos(small_fish[j][2][0]) * small_fish[j][1][1];
 
-            small_fish[j][2][0] += small_fish[j][2][1]; //\omega = \omega + \alpha
+            //\omega = \omega + \alpha
+            small_fish[j][2][0] += small_fish[j][2][1];
         }
 
+        // フロッキング処理(\alphaを決める処理)
+
+
+        // ファイル出力
         char fname[50];
         sprintf(fname, "dat/%d.dat", i);
         FILE* fp = fopen(fname, "w");
@@ -35,6 +48,7 @@ int main(int argc, char* argv[])
         }
         for (int j = 0; j < fish_amount; j++) {
             if (fabs(small_fish[j][0][0]) < field_x && fabs(small_fish[j][0][1] < field_y)) {
+                // 描画範囲内のもののみ出力
                 fprintf(fp, "%f, %f, %f, %f, x, y, v_x, v_y\n", small_fish[j][0][0], small_fish[j][0][1], small_fish[j][1][0], small_fish[j][1][1]);
             }
         }
