186a866abe034e21bf12fd1875adb2a2dd1d5aac
Actat <akiotoshima@gmail.com>
2016-11-09 18:11:28 +0900

    new file:   flocking.sh プログラムを実行するためのスクリプト. コンパイルする部分を書いた.

---
+++ flocking.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+rm main.out
+rm -rf dat
+mkdir dat
+
+if [ -f main.c ]; then
+    echo "コンパイルします."
+    gcc -o main.out main.c -lm
+else
+    echo "main.c が存在しないようです."
+    exit 0
+fi
+
+if [ -f main.out ]; then
+    echo "コンパイルに成功したようです."
+#    echo "プログラムを実行し、連番ファイルを出力します."
+else
+    echo "コンパイルに失敗したようです."
+    exit 0
+fi
+
