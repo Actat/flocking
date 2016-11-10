dd3bc7f5adf0dd71a28addf8c029c4f6033474b2
Actat <akiotoshima@gmail.com>
2016-11-10 14:28:19 +0900

    modified:   flocking.sh 静止画を作る部分

--- flocking.sh
+++ flocking.sh
@@ -9,6 +9,8 @@ fish_amount=50
 
 rm main.out
 rm -rf dat
+rm -rf eps
+rm -rf png
 mkdir dat
 
 if [ -f main.c ]; then
@@ -23,9 +25,46 @@ if [ -f main.out ]; then
     echo "コンパイルに成功したようです."
     echo "プログラムを実行し、連番ファイルを出力します."
     ./main.out $frame_number $field_x $field_y $fish_amount
+    echo "プログラムの実行が終了しました。"
 else
     echo "コンパイルに失敗したようです."
     exit 0
 fi
 
 rm main.out
+
+echo "gnuplotで画像ファイルに変換します."
+mkdir eps
+touch plotimg.gp
+i=0
+while [ $i -lt $frame_number ]
+do
+    if [ -e dat/${i}.dat ]; then
+        echo "set encoding iso_8859_1" >> plotimg.gp
+        echo "set term postscript color eps enhanced" >> plotimg.gp
+        echo "set output 'eps/$i.eps'" >> plotimg.gp
+        echo "set size ratio 1" >> plotimg.gp
+        echo "set palette rgb 33,13,10" >> plotimg.gp
+        echo "set xrange[-$field_x:$field_x]" >> plotimg.gp
+        echo "set yrange[-$field_y:$field_y]" >> plotimg.gp
+        echo "plot 'dat/${i}.dat' u 1:2:3:4:(sqrt(\$3*\$3+\$4*\$4)) w vector lc palette ti ''" >> plotimg.gp
+    else
+        echo "${i}は存在しないです."
+    fi
+    gnuplot plotimg.gp
+    rm plotimg.gp
+    i=`expr $i + 1`
+done
+
+mkdir png
+echo "epsからpngに変換します."
+i=0
+while [ $i -lt $frame_number ]
+do
+    if [ -e eps/${i}.eps ]; then
+        convert eps/${i}.eps png/${i}.png
+    else
+        echo "eps/${i}.eps は存在しません."
+    fi
+    i=`expr $i + 1`
+done
