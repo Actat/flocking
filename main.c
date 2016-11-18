85d78759605a06fc703598e798bde3d3ad1b8900
Actat <akiotoshima@gmail.com>
2016-11-19 02:41:30 +0900

    modified:   main.c centroidを予想される重心から現在の重心に変更

--- main.c
+++ main.c
@@ -64,8 +64,8 @@ int main(int argc, char* argv[])
                     relative_posi_vect_theta = atan2(relative_posi_vect_y, relative_posi_vect_x);
                     if (relative_posi_vect_r < view_r && relative_posi_vect_theta - small_fish[j][0][2] < view_theta) {
                         fish_in_view++;
-                        centroid_x += small_fish[k][0][0] + cos(small_fish[k][0][2]) * small_fish[k][1][0];
-                        centroid_y += small_fish[k][0][1] + sin(small_fish[k][0][2]) * small_fish[k][1][0];
+                        centroid_x += small_fish[k][0][0];
+                        centroid_y += small_fish[k][0][1];
                     }
                 }
             }
