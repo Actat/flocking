3bb3c4133497b808529c316043c9b63e242ccefd
Actat <akiotoshima@gmail.com>
2016-11-19 04:36:18 +0900

    modified:   main.c 描画領域の外の方まで行ってしまう問題の解決

--- main.c
+++ main.c
@@ -88,6 +88,38 @@ int main(int argc, char* argv[])
             }
             // \alphaを決める
             small_fish[j][2][1] = 10 * (centroid_theta - small_fish[j][0][2]) + 100 * ave_theta;
+
+            // 範囲外に出そうになったらUターンする
+            if (fabs(small_fish[j][0][0]) > 40 || fabs(small_fish[j][0][1]) > 40) {
+                if (small_fish[j][0][0] > 40) {
+                    if (small_fish[j][0][2] > 0) {
+                        small_fish[j][2][1] += 0.01;
+                    } else {
+                        small_fish[j][2][1] += -0.01;
+                    }
+                }
+                if (small_fish[j][0][0] < -40) {
+                    if (small_fish[j][0][2] > 0) {
+                        small_fish[j][2][1] += -0.01;
+                    } else {
+                        small_fish[j][2][1] += 0.01;
+                    }
+                }
+                if (small_fish[j][0][1] > 40) {
+                    if (small_fish[j][0][2] > M_PI / 2 || small_fish[j][0][2] < -1 * M_PI / 2) {
+                        small_fish[j][2][1] += 0.01;
+                    } else {
+                        small_fish[j][2][1] += -0.01;
+                    }
+                }
+                if (small_fish[j][0][1] < -40) {
+                    if (small_fish[j][0][2] > M_PI / 2 || small_fish[j][0][2] < -1 * M_PI / 2) {
+                        small_fish[j][2][1] += -0.01;
+                    } else {
+                        small_fish[j][2][1] += 0.01;
+                    }
+                }
+            }
         }
 
         // 移動の処理
